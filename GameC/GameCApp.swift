6d62544a00b7c0037dc1d10d760e716c5b913d07
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-11-24 16:26:07 +0300

    Initial Commit

---
+++ GameC/GameCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCApp.swift
+//  GameC
+//
+//  Created by Ghalia Mohammed Al Muaddi on 10/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct GameCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
