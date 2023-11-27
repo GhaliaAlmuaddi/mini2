064778b2cd1ad5b245980be9ea66360b5d77c64d
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-11-27 11:53:29 +0300

    adding the PopUp view

--- GameC/ContentView.swift
+++ GameC/ContentView.swift
@@ -7,18 +7,67 @@
 
 import SwiftUI
 
+   
 struct ContentView: View {
+    @State var isActive: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+                    //BACKGROUND START...
+                    AngularGradient(
+                        gradient: Gradient(colors: [ CustomColor.CustomDpurple,CustomColor.CustomLpurple]), center: .topLeading,
+                        startAngle: .zero,
+                        endAngle: .degrees(100)
+                        
+                    )
+                    .ignoresSafeArea()
+                    //BACKGROUND END...
+            VStack {
+                Button {
+                    isActive = true
+                } label: {
+                    Text("يلا نلعب")
+                }
+            }
+            .padding(30)
+
+            if isActive {
+                CreatingTeamDialogue(isActive: $isActive, title: "كون فريقك", buttonTitle: "يلا نبدا") {
+                   // print("Pass to viewModel")
+                }
+            }
         }
-        .padding()
+
+//        NavigationStack {
+//            
+//           VStack {
+//               NavigationLink(destination: {
+//                   //PopupView()
+//               }{
+//                   
+//                   Circle().fill(Color.orange).frame(width: 100, height: 100, alignment: .center)}
+//                    
+//                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/ ){
+//                        Circle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 100, alignment: .center)
+//                    })
+//                    
+//                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/ ) {
+//                        Circle().fill(Color.pink).frame(width: 100, height: 100, alignment: .center)
+//                    }
+//           }.navigationTitle("home page").padding()
+//            }
+        }
     }
+// CUSTOM COLORS START...
+struct CustomColor {
+    static let CustomDpurple = Color("CustomDpurple")
+    static let CustomDyellow = Color("CustomDyellow")
+    static let CustomLpurple = Color("CustomLpurple")
+    static let CustomLyellow = Color("CustomLyellow")
+    
 }
 
+// CUSTOM COLORS END..
 #Preview {
     ContentView()
 }
---
+++ GameC/CreatingTeamDialogue.swift
@@ -0,0 +1,173 @@
+//
+//  JoiningDialogue.swift
+//  GameC
+//
+//  Created by Ghalia Mohammed Al Muaddi on 10/05/1445 AH.
+//
+
+import SwiftUI
+
+
+    struct CreatingTeamDialogue: View {
+        @Binding var isActive: Bool
+
+        let title: String
+        //let message: String
+        @State private var team1Name = ""
+        @State private var team2Name = ""
+        let buttonTitle: String
+        let action: () -> ()
+        @State private var offset: CGFloat = 3000
+        
+        var objectCripto : [String]=["a","b","c","E","F","G","H","I","J","K"]
+        
+        
+        
+        
+        var body: some View {
+          
+            
+           //var objectCripto_shaffle = Array_shaffle(array1: objectCripto)
+            
+//           ForEach(objectCripto,id: \.self){ element in
+//            Text("\(element)")
+//          }
+//            var indx=3
+//            var res = Array_shaffle2(array1: objectCripto,indexP: indx)
+//            Text("\(res)").background(.black)
+            
+          //  print("Pass to viewModel")
+            
+            ZStack {
+                Color(.black)
+                    .blur(radius:100)
+                    .opacity(0.2)
+                    .onTapGesture {
+                        close()
+                    }
+                    Rectangle()
+                    .fill(.white.opacity(0.09)).frame(width: 470, height:450).cornerRadius(13)
+                    //.blur(radius:40)
+                
+//               Rectangle()
+//                    .frame(width: 470, height:450 , alignment: .center)
+//                    //.cornerRadius(20)
+//                  .background(.white)
+//                    .opacity(0.2).blur(radius:40)
+//       
+//                 .overlay {
+//                
+//                     LinearGradient(colors: [.clear, .white.opacity(0.05)], startPoint: .top, endPoint: .bottom)
+//               }
+            //shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 2)
+              // shadow(color: .white, radius: 5, x: 0, y: -2)
+                VStack {
+                    Text(title)
+                        .font(.title2)
+                        .bold().foregroundStyle(.white)
+                        .padding(30)
+
+                   // VStack {
+                    
+                    TextField("اسم الفريق الاول", text: $team1Name).frame(width:352.53,height: 83).overlay(
+                        RoundedRectangle(cornerRadius: 13)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
+                        .multilineTextAlignment(.center)
+                    TextField("اسم الفريق الثاني", text: $team2Name).frame(width:352.53,height: 83).overlay(
+                        RoundedRectangle(cornerRadius: 13)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
+                        .multilineTextAlignment(.center)
+                        //   }
+                        .font(.body)
+
+                    Button {
+                        action()
+                        close()
+                    } label: {
+                        ZStack {
+                           
+//                            AngularGradient(
+//                                gradient: Gradient(colors: [ CustomColor.CustomDpurple,CustomColor.CustomLpurple]), center: .topLeading,
+//                                startAngle: .zero,
+//                                endAngle: .degrees(100)
+                                
+                            
+                            LinearGradient(colors: [ CustomColor.CustomDyellow,CustomColor.CustomLyellow], startPoint:.bottom, endPoint: .top).frame(width: 286, height:71).cornerRadius(41)
+//                        RoundedRectangle(cornerRadius: 41)
+//                                .foregroundColor(.yellow)
+
+                            Text(buttonTitle)
+                                .font(.system(size: 22, weight: .bold))
+                                .foregroundColor(.white)
+                                .padding(30)
+                        }
+                        .padding(30)
+                    }
+                }.cornerRadius(20)
+           
+                //.fixedSize(horizontal: false, vertical: true)
+                .padding(30)//.frame(width: 470, height:508 , alignment: .center)
+                
+                //.background(.clear).opacity(10).cornerRadius(20)
+//                .clipShape(RoundedRectangle(cornerRadius: 20)).background()
+                .overlay(alignment: .trailingFirstTextBaseline) {
+                    Button {
+                        close()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.title2)
+                            .fontWeight(.medium)
+                            .foregroundColor(.white)
+                    }
+                    .tint(.black)
+                    .padding()
+                }
+                .shadow(radius: 20)
+                .padding(30)
+                .offset(x: 0, y: offset)
+                .onAppear {
+                    withAnimation(.spring()) {
+                        offset = 0
+                    }
+                }
+            }.ignoresSafeArea()
+        }
+        
+//        func Array_shaffle (array1:[String])-> [String] {
+//          var array1=array1.shuffled()
+//            return array1
+//        }
+//        
+//        func Array_shaffle2 (array1:[String], indexP : Int)-> String {
+//          var array1=array1.shuffled()
+//           var index2=0
+//            var value = ""
+//            for i in array1 {
+//                if(index2 == indexP){
+//                var value = i
+//                    break
+//                }
+//                index2=index2+1
+//            }
+//return value
+//        }
+
+        func close() {
+            withAnimation(.spring()) {
+                offset = 1000
+                isActive = false
+            }
+        }
+    }
+
+    struct CustomDialog_Previews: PreviewProvider {
+        static var previews: some View {
+            CreatingTeamDialogue(isActive: .constant(true), title: "كون فريقك", buttonTitle: "يلا نبدا", action: {})
+        }
+    }
+
+//#Preview {
+//    JoiningDialogue()
+//}
