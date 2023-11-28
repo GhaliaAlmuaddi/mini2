4b74fe641dff1cb74a9b6ceb08daa4b07eebb627
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-11-28 09:33:16 +0300

    hi

---
+++ GameC/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  homepage.swift
+//  GameC
+//
+//  Created by Ghalia Mohammed Al Muaddi on 13/05/1445 AH.
+//
+
+import SwiftUI
+
+struct homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    homepage()
+}
